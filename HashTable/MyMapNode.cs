adcc24b80316220fe696fd2c38df5d7dd0145e25
rani1029 <ranig1029@gmail.com>
2021-08-23 23:37:38 +0530

    frequency of words uc added

---
+++ HashTable/MyMapNode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    class MyMapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        //constructor
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        public V Get(K Key)
+        {
+            int position = GetArrayPosition(Key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(Key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        public struct KeyValue<K, V>
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+        }
+    }
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to hashtable ");
+            MyMapNode<string, string> myMapNode = new MyMapNode<string, string>(5);
+
+            //Add data
+            myMapNode.Add("0", "To");
+            myMapNode.Add("1", "be");
+            myMapNode.Add("2", "To");
+            myMapNode.Add("3", "or");
+            myMapNode.Add("4", "not");
+            myMapNode.Add("5", "be");
+
+            //Get Value and Display
+            string hash5 = myMapNode.Get("5");
+            Console.WriteLine("5th index  value: " + hash5);
+
+
+            string hash2 = myMapNode.Get("2");
+            Console.WriteLine("2nd index  value: " + hash2);
+        }
+    }
+}
+
